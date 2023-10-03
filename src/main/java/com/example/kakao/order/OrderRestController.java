f01f136cdd4a9bef85b3008a03ac7aaf64bcd79a
muaga <more9494@naver.com>
2023-10-03 15:57:09 +0900

    기능 3 구현 중

--- src/main/java/com/example/kakao/order/OrderRestController.java
+++ src/main/java/com/example/kakao/order/OrderRestController.java
@@ -39,18 +39,18 @@ public class OrderRestController {
         if (sessionUser == null) {
             throw new Exception401("인증되지 않았습니다");
         }
-        OrderResponse.FindByIdDTO responseDTO = orderService.findById(id, 1);
+        OrderResponse.FindByIdDTO responseDTO = orderService.findById(id, sessionUser.getId());
         return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
 
     // (기능4) 주문상품 정보조회 (유저별) - 장바구니 내역 가져오기
     @GetMapping("/orders")
     public ResponseEntity<?> findAllByUser() {
-        // User sessionUser = (User) session.getAttribute("sessionUser");
-        // if (sessionUser == null) {
-        // throw new Exception401("인증되지 않았습니다.");
-        // }
-        OrderResponse.FindAllByUserDTO responseDTO = orderService.findAllByUser(1);
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        if (sessionUser == null) {
+            throw new Exception401("인증되지 않았습니다.");
+        }
+        OrderResponse.FindAllByUserDTO responseDTO = orderService.findAllByUser(sessionUser.getId());
         return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
 
